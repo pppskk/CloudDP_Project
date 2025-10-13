4fb42a8f790b1dc622d3a8797350de917dc64d3e
pppskk <namthip_m@cmu.ac.th>
2025-10-13 16:19:24 +0700

    Add Docker support and update unit tests for Utils functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3005;
+const port = 80;
 
 // หน้า index
 app.get("/", (req, res) => {
--- tests/unit_test.js
+++ tests/unit_test.js
@@ -2,19 +2,35 @@ const Utils = require("../src/Utils");
 
 const unit_test = async () => {
     
-    if (Utils.add(2, 3) === 5) {
-        console.log(0);
-    }
-    else {
-        console.log(1);
+    // if (Utils.add(2, 3) !== 5) {
+    //     console.error(" add test failed");
+    //     process.exit(1);
+    // }
+    let failed = false;
+
+    if (Utils.add(2, 3) !== 5) {
+        console.error("add test failed");
+        failed = true;
     }
 
-    if (Utils.add(2, 4) === 6) {
-        console.log(0);
-        // helloworld();
+    if (Utils.add(2, 8) !== 5) {
+        console.error("add test failed");
+        failed = true;
     }
-    else {
-        console.log(2);
+
+    // if (Utils.add(2, 3) === 5) {
+    //     console.log(0);
+    // }
+    // else {
+    //     console.log(1);
+    // }
+
+    if (failed) {
+        console.error("Some tests failed");
+        process.exit(1);
+      } else {
+        console.log("All tests passed!");
+        process.exit(0);
     }
 }
 
